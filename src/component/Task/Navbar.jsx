f7b14e7e2cf6fbfae08c9cbb871bf69f3906850d
Madhavan-12 <mwaran934@gmail.com>
2025-09-24 15:07:17 +0800

    Navbar.jsx bg colur changed

--- src/component/Task/Navbar.jsx
+++ src/component/Task/Navbar.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 p-4 text-white flex justify-between">
-      <h1 className="font-bold text-xl">MyShop</h1>
-      <ul className="flex gap-4">
-        <li>Home</li>
-        <li>Component</li>
-        <li>Rendering</li>
-        <li>Cooks</li>
-        <li>Product</li>
+      <h1 className="font-bold text-xl hover:text-amber-200">MyShop</h1>
+      <ul className="flex gap-4 hover:text-violet-500">
+        <li className='hover:text-violet-950'>Home</li>
+        <li className='hover:text-violet-950'>Component</li>
+        <li className='hover:text-violet-950'>Rendering</li>
+        <li className='hover:text-violet-950'>Cooks</li>
+        <li className='hover:text-violet-950'>Product</li>
       </ul>
     </nav>
   );
